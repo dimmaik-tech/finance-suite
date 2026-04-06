68b706777fdb4023500da10369a5bfef88f64da0
Dim Leb <dimmaik@gmail.com>
2026-04-06 20:00:23 +0300

    Update 1_Bond_Calculator.py

--- pages/1_Bond_Calculator.py
+++ pages/1_Bond_Calculator.py
@@ -12,7 +12,7 @@ show_logout_button(key="logout_bond")
 
 st.title("📊 Υπολογιστής Ομολόγων Pro")
 
-with open("bond_calculator.html", "r", encoding="utf-8") as f:
+with open("bond v6.html", "r", encoding="utf-8") as f:
     html_code = f.read()
 
 components.html(html_code, height=1000, scrolling=True)
